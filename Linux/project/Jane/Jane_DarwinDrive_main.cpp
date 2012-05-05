dbafcdc3c71527d2ed0dda22d6c58853b06f22f9
Chunyang Sun <suncy0012@gmail.com>
2012-05-05 12:44:33 -0400

    Changes made by jane to get syncwrite working

--- Linux/project/Jane/Jane_DarwinDrive_main.cpp
+++ Linux/project/Jane/Jane_DarwinDrive_main.cpp
@@ -28,12 +28,22 @@
 #include "LinuxMotionTimer.h"
 #include "LinuxCM730.h"
 #include "LinuxActionScript.h"
-#include "Jane_StatusCheck.h"
-//#include "Jane_DriveVision.h"
+
+//opencv headers
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sys/un.h>
+#include "cv.h"
+#include "highgui.h"
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <math.h>
 
 #include "Steer.h"
-#include "RoadTracker.h"
-#include "ImgProcess.h"
+
 
 #ifdef MX28_1024
 #define MOTION_FILE_PATH    "../../../Data/motion_1024.bin"
@@ -42,7 +52,6 @@
 #endif
 
 #define INI_FILE_PATH       "../../../Data/config.ini"
-#define SCRIPT_FILE_PATH    "script.asc"
 
 #define U2D_DEV_NAME0       "/dev/ttyUSB0"
 #define U2D_DEV_NAME1       "/dev/ttyUSB1"
@@ -117,50 +126,11 @@ int main(void)
     signal(SIGQUIT, &sighandler);
     signal(SIGINT, &sighandler);
 
+//???
     change_current_dir();
-
+//???
     minIni* ini = new minIni(INI_FILE_PATH);
-    Image* rgb_output = new Image(Camera::WIDTH, Camera::HEIGHT, Image::RGB_PIXEL_SIZE);
-    Image* grey_output = new Image(Camera::WIDTH, Camera::HEIGHT, Image::GREY_PIXEL_SIZE);;
 
-    LinuxCamera::GetInstance()->Initialize(0);
-    LinuxCamera::GetInstance()->SetCameraSettings(CameraSettings());    // set default
-    LinuxCamera::GetInstance()->LoadINISettings(ini);                   // load from ini
-    
-    
-    mjpg_streamer* streamer = new mjpg_streamer(Camera::WIDTH, Camera::HEIGHT);
-
-    ColorFinder* ball_finder = new ColorFinder();
-    ball_finder->LoadINISettings(ini);
-    httpd::ball_finder = ball_finder;
- 
-    BallTracker tracker = BallTracker();
-    BallFollower follower = BallFollower();
-
-//ColorFinder(hue, hue_tol, min_saturation, min_value, min_per, max_per)
-    ColorFinder* red_finder = new ColorFinder(0, 15, 45, 0, 0.3, 50.0); 
-
-    red_finder->LoadINISettings(ini, "RED");
-    httpd::red_finder = red_finder;
-
-    ColorFinder* yellow_finder = new ColorFinder(60, 15, 45, 0, 0.3, 50.0);
-    yellow_finder->LoadINISettings(ini, "YELLOW");
-    httpd::yellow_finder = yellow_finder;
-
-    ColorFinder* blue_finder = new ColorFinder(225, 15, 45, 0, 0.3, 50.0);
-    blue_finder->LoadINISettings(ini, "BLUE");
-    httpd::blue_finder = blue_finder;
-	 
-    ColorFinder* road_finder = new ColorFinder(15, 15, 0, 0, 0.3, 50.0); //look for road in black
-    road_finder->LoadINISettings(ini);
-    httpd::road_finder = road_finder;
-
-    RoadTracker roadtracker = RoadTracker();
-    Steer steer;
-    ImgProcess imgprocess = ImgProcess();
-    //steer.currentImage = rgb_output;
-    //steer.currentImage = grey_output;
-    httpd::ini = ini;
 
     //////////////////// Framework Initialize ////////////////////////////
     if(MotionManager::GetInstance()->Initialize(&cm730) == false)
@@ -215,91 +185,66 @@ int main(void)
     else
         exit(0);
 
+//opencv camera initialization
+        CvCapture* capture = cvCaptureFromCAM( 0 );
+  	if( !capture ) {
+    	fprintf( stderr, "ERROR: capture is NULL \n" );
+    	getchar();
+   	return -1;
+ 	}
+   
+   IplImage image;
+
+   image.width = 320;
+   image.depth = 240;
+ 
+// Create a window in which the captured images will be presented
+   cvNamedWindow( "mywindow", CV_WINDOW_AUTOSIZE );
+
+   int p;
+
+  
+
+
     Action::GetInstance()->m_Joint.SetEnableBody(true, true);			
     MotionManager::GetInstance()->SetEnable(true);//False: motor turns off
-
-    cm730.WriteByte(CM730::P_LED_PANNEL, 0x01|0x02|0x04, NULL); //head LED turns yellow, camera on
 
     Action::GetInstance()->Start(1); //standing up
     while(Action::GetInstance()->IsRunning()) usleep(8*1000);
 
 
-	pthread_t thread_t;
+    pthread_t thread_t;
     pthread_create(&thread_t, NULL, pedal_thread, NULL);
-
-    while(1)
-    {
-        StatusCheck::Check(cm730);
-
-        Point2D ball_pos, red_pos, yellow_pos, blue_pos, road_pos;
-
-
-		///////////add vision here///////////////////////////////////////////////////////////////////
-        LinuxCamera::GetInstance()->CaptureFrame(); 
-        memcpy(rgb_output->m_ImageData, LinuxCamera::GetInstance()->fbuffer->m_RGBFrame->m_ImageData, 		   		LinuxCamera::GetInstance()->fbuffer->m_RGBFrame->m_ImageSize);
-	//ImgProcess::RGBtoGREY(LinuxCamera::GetInstance()->fbuffer); //called in image.cpp, so we dont need to call here.
-	//grey_output = LinuxCamera::GetInstance()->fbuffer->m_GREYFrame;
-  memcpy(grey_output->m_ImageData, LinuxCamera::GetInstance()->fbuffer->m_GREYFrame->m_ImageData, 		   		LinuxCamera::GetInstance()->fbuffer->m_GREYFrame->m_ImageSize);
-	
-	
-	if(StatusCheck::m_cur_mode == DRIVE)
-	{ 
-	    //imgprocess.Erosion(rgb_output);
-	    //ImgProcess::Dilation(rgb_output);
-	    //roadtracker.Process(road_finder->GetPosition(LinuxCamera::GetInstance()->fbuffer->m_HSVFrame));
-
-	  //  for(int i = 0; i < rgb_output->m_NumberOfPixels; i++)
-            //{
-		//if(road_finder->m_result->m_ImageData[i] == 1)
-		//{
-				//rgb_output->m_ImageData[i*rgb_output->m_PixelSize + 0] = 128;
-                   		//rgb_output->m_ImageData[i*rgb_output->m_PixelSize + 1] = 255;
-                   		//rgb_output->m_ImageData[i*rgb_output->m_PixelSize + 2] = 128;
-		 //}  
-	   //}
-	   //steer.Process();
+     
+    //steer.Process();
           
-			//rgb_output->m_ImageData[(int) Steer().center*rgb_output->m_PixelSize + 0] = 255;
-                   	//rgb_output->m_ImageData[(int) Steer().center*rgb_output->m_PixelSize + 1] = g;
-                   	//rgb_output->m_ImageData[(int) Steer().center*rgb_output->m_PixelSize + 2] = b;
-        } //else if: DRIVE
-      
-
-        streamer->send_image(grey_output);
-        //streamer->send_image(rgb_output);
 
 
-        if(StatusCheck::m_is_started == 0)
-            continue;
-
-        switch(StatusCheck::m_cur_mode)
-        {
-        case READY:
-            break;
-       
-	case DRIVE:
-	     {		
+ 	int i = 0;
+    while(1){
+		// Get one frame
+		IplImage* frame = cvQueryFrame( capture );
+		if( !frame ) {
+		      fprintf( stderr, "ERROR: frame is null...\n" );
+		      getchar();
+		      break;
+		}
+		cvShowImage( "mywindow", frame );
+		cvReleaseCapture( &capture );
 		
-		//printf("ID7 %d", Action::GetInstance()->m_Joint.GetCCWSlope(7));
-		//printf("HERE");
+
+
+		printf("HERE");
 		//MotionManager::GetInstance()->SetEnable(false);
-	  		
-		//MotionManager::GetInstance()->SetEnable(true);
-   				 
-
-		//	Action::GetInstance()->m_Joint.SetValue(1,2500);
-		//	Action::GetInstance()->m_Joint.SetValue(2,1500);
-               		
-
-	   
-	                //Walking::GetInstance()->m_Joint.SetEnableBodyWithoutHead(true, true);
+  		int iniPos = Action::GetInstance()->m_Joint.GetValue(3);
+		
+		if (i<50){
+				Action::GetInstance()->m_Joint.SetValue(1,iniPos+ 4*i);
+				//Action::GetInstance()->m_Joint.SetValue(2,2000);
+				i++;
+		}	            
 			
-	    }
-
-	    break;
-	
-           }
-       }
+     	     }   
     
     return 0;
 }
